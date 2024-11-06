d82da06707333afae425d5dedafe5e03a421440c
Tarcisio Couto <tarcisiocouto@MacBook-Air-de-Tarcisio.local>
2024-11-06 15:32:22 -0300

    Início do novo histórico

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
 import express from 'express';
 import transactionsGet from './routes/transactionsGet';
 import transactionsPost from './routes/transactionsPost';
+import cors from 'cors';
+
+const allowedOrigins = ['https://dt-money-webproject.vercel.app', 'http://localhost:3000'];
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+  
 
 app.get('/transactions', transactionsGet);
 app.post('/transactions', transactionsPost);
